13d9fe0468d0967e8ed7e66adee729a9b3a77f48
groberts <gtr22@macmini.local>
2016-09-16 11:39:03 +0200

    Added a log message on app start, and a button with another log message.

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var DoItButton: UIButton!
 
+    @IBAction func Click(_ sender: AnyObject) {
+        NSLog("click")
+    }
+    @IBAction func Touch(_ sender: AnyObject) {
+        NSLog("Touch")
+    }
 }
 
