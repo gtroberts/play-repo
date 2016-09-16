86c66dfaff3133962eb9a7624b7c08c5d90d94a9
groberts <gtr22@macmini.local>
2016-09-16 12:10:15 +0200

    Added MapViewer class

---
+++ GitUseExample/MapViewer.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewer.swift
+//  GitUseExample
+//
+//  Created by groberts on 16/09/2016.
+//  Copyright © 2016 groberts. All rights reserved.
+//
+
+import Foundation
+
+class MapViewer: NSObject {
+    
+}
